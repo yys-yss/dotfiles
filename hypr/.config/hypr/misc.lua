2558e02fe09b1246e1019436b9831c338d1c84d4
Yousef <yousefyassersalah@gmail.com>
2026-06-08 18:31:41 +0300

    hyprland: enable vrr for fullscreen applications

--- hypr/.config/hypr/misc.lua
+++ hypr/.config/hypr/misc.lua
@@ -1,6 +1,7 @@
 hl.config({
 	misc = {
-		force_default_wallpaper = -1, -- Set to 0 or 1 to disable the anime mascot wallpapers
+		force_default_wallpaper = 0, -- Set to 0 or 1 to disable the anime mascot wallpapers
 		disable_hyprland_logo = true, -- If true disables the random hyprland logo / anime girl background. :(
+		vrr = 2,
 	},
 })
