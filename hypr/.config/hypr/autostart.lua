f54e5251a5f4843edf360cb14d285dd264f3b675
Yousef <yousefyassersalah@gmail.com>
2026-06-08 18:29:34 +0300

    hyprland: change cursor and remove hyperpaper

--- hypr/.config/hypr/autostart.lua
+++ hypr/.config/hypr/autostart.lua
@@ -1,11 +1,10 @@
-local terminal    = "kitty"
-local fileManager = "dolphin"
-local menu        = "pkill rofi || bash ~/.config/rofi/launchers/type-4/launcher.sh"
+local terminal = "kitty"
 
-hl.on("hyprland.start", function () 
-  hl.exec_cmd("hypridle")
-  hl.exec_cmd("nm-applet")
-  hl.exec_cmd(terminal)
-  hl.exec_cmd("awww-daemon")
-  hl.exec_cmd("waybar & hyprpaper")
+hl.on("hyprland.start", function()
+	-- hl.exec_cmd("hypridle")
+	hl.exec_cmd("settings set org.gnome.desktop.interface cursor-theme BreezeX-RosePine-Linux")
+	hl.exec_cmd("nm-applet")
+	hl.exec_cmd(terminal)
+	hl.exec_cmd("awww-daemon")
+	hl.exec_cmd("waybar")
 end)
--- hypr/.config/hypr/env.lua
+++ hypr/.config/hypr/env.lua
@@ -1,3 +1,4 @@
 hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_THEME", "MyCursor")
+hl.env("XCURSOR_THEME", "BreezeX-RosePine-Linux")
+hl.env("HYPRCURSOR_THEME", "rose-pine-hyprcursor")
 hl.env("HYPRCURSOR_SIZE", "24")
