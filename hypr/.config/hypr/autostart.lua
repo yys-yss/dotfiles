5e772812637ed4f0761d7b10d92f389f89c4e53c
Yousef <yousefyassersalah@gmail.com>
2026-06-10 23:37:56 +0300

    hyprland: remove nm applet

--- hypr/.config/hypr/autostart.lua
+++ hypr/.config/hypr/autostart.lua
@@ -3,7 +3,6 @@ local terminal = "kitty"
 hl.on("hyprland.start", function()
 	-- hl.exec_cmd("hypridle")
 	hl.exec_cmd("settings set org.gnome.desktop.interface cursor-theme BreezeX-RosePine-Linux")
-	hl.exec_cmd("nm-applet")
 	hl.exec_cmd(terminal)
 	hl.exec_cmd("awww-daemon")
 	hl.exec_cmd("waybar")
