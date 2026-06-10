cac5c96a2a039ac38f955ef676274def60d3cfb0
Yousef <yousefyassersalah@gmail.com>
2026-06-10 23:37:42 +0300

    terminal: add shutdown and reboot abbrs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,10 @@ export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
 alias fishconfig="nvim ~/.config/fish/config.fish"
 export XDG_DATA_DIRS="$HOME/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:$XDG_DATA_DIRS"
 
+# Reboot and shutdown
+abbr reboot "uwsm-app -- hyprshutdown -t 'Restarting...' --post-cmd 'reboot'"
+abbr shutdown "uwsm-app -- hyprshutdown -t 'Shutting down...' --post-cmd 'shutdown -P now'"
+
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
