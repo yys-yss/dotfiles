af6a91845ef2a121afcf423c7ad6c2e1ce1ae00a
Yousef <yousefyassersalah@gmail.com>
2026-06-10 23:38:37 +0300

    hyprland: fix crash when rebooting

--- hypr/.config/hypr/env.lua
+++ hypr/.config/hypr/env.lua
@@ -2,3 +2,7 @@ hl.env("XCURSOR_SIZE", "24")
 hl.env("XCURSOR_THEME", "BreezeX-RosePine-Linux")
 hl.env("HYPRCURSOR_THEME", "rose-pine-hyprcursor")
 hl.env("HYPRCURSOR_SIZE", "24")
+
+-- Fix for crash due to monitor
+hl.env("AQ_NO_ATOMIC", "1")
+hl.env("AQ_DRM_DEVICES", "/dev/dri/card1")
--- hypr/.config/hypr/monitors.lua
+++ hypr/.config/hypr/monitors.lua
@@ -1,8 +1,8 @@
 hl.monitor({
-	output = "",
-	mode = "2560x1440@240",
-	position = "auto",
-	scale = "auto",
+	output = "DP-3",
+	mode = "2560x1440@239.97",
+	position = "0x0",
+	scale = 1,
 	supports_hdr = 0,
 	cm = "auto",
 })
